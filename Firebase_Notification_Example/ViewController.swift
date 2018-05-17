c745337cc37f9779d330c4e7ee9c2d00be7bab72
TuanLeo <tuanleo@b120709-mb.local>
2018-05-17 15:47:01 +0700

    Initial Commit

---
+++ Firebase_Notification_Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Firebase_Notification_Example
+//
+//  Created by TuanLeo on 5/17/18.
+//  Copyright © 2018 TuanLeo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
